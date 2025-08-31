a1d9bc76cc196192119ae319d227b4245c806eed
bbfox0703 <101542129+bbfox0703@users.noreply.github.com>
2025-08-31 18:07:45 +0800

    relayout

--- ZZ_Tools/AAToggleGenerator/MainForm.Designer.cs
+++ ZZ_Tools/AAToggleGenerator/MainForm.Designer.cs
@@ -29,58 +29,58 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.btnAAToggleGen = new System.Windows.Forms.Button();
-            this.btnIDRenumber = new System.Windows.Forms.Button();
-            this.btnReplaceProcName = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            btnAAToggleGen = new System.Windows.Forms.Button();
+            btnIDRenumber = new System.Windows.Forms.Button();
+            btnReplaceProcName = new System.Windows.Forms.Button();
+            SuspendLayout();
             // 
             // btnAAToggleGen
             // 
-            this.btnAAToggleGen.Location = new System.Drawing.Point(41, 13);
-            this.btnAAToggleGen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btnAAToggleGen.Name = "btnAAToggleGen";
-            this.btnAAToggleGen.Size = new System.Drawing.Size(371, 45);
-            this.btnAAToggleGen.TabIndex = 0;
-            this.btnAAToggleGen.Text = "Generate AA bulk toggle script";
-            this.btnAAToggleGen.UseVisualStyleBackColor = true;
-            this.btnAAToggleGen.Click += new System.EventHandler(this.btnAAToggleGen_Click);
+            btnAAToggleGen.Location = new System.Drawing.Point(13, 13);
+            btnAAToggleGen.Margin = new System.Windows.Forms.Padding(4);
+            btnAAToggleGen.Name = "btnAAToggleGen";
+            btnAAToggleGen.Size = new System.Drawing.Size(300, 26);
+            btnAAToggleGen.TabIndex = 0;
+            btnAAToggleGen.Text = "Generate AA bulk toggle script";
+            btnAAToggleGen.UseVisualStyleBackColor = true;
+            btnAAToggleGen.Click += btnAAToggleGen_Click;
             // 
             // btnIDRenumber
             // 
-            this.btnIDRenumber.Location = new System.Drawing.Point(41, 65);
-            this.btnIDRenumber.Name = "btnIDRenumber";
-            this.btnIDRenumber.Size = new System.Drawing.Size(371, 45);
-            this.btnIDRenumber.TabIndex = 1;
-            this.btnIDRenumber.Text = "Reassign .CT <ID> tag";
-            this.btnIDRenumber.UseVisualStyleBackColor = true;
-            this.btnIDRenumber.Click += new System.EventHandler(this.btnIDRenumber_Click);
+            btnIDRenumber.Location = new System.Drawing.Point(13, 46);
+            btnIDRenumber.Name = "btnIDRenumber";
+            btnIDRenumber.Size = new System.Drawing.Size(300, 26);
+            btnIDRenumber.TabIndex = 1;
+            btnIDRenumber.Text = "Reassign .CT <ID> tag";
+            btnIDRenumber.UseVisualStyleBackColor = true;
+            btnIDRenumber.Click += btnIDRenumber_Click;
             // 
             // btnReplaceProcName
             // 
-            this.btnReplaceProcName.Location = new System.Drawing.Point(41, 116);
-            this.btnReplaceProcName.Name = "btnReplaceProcName";
-            this.btnReplaceProcName.Size = new System.Drawing.Size(371, 45);
-            this.btnReplaceProcName.TabIndex = 2;
-            this.btnReplaceProcName.Text = "Replace AOB .exe to $process";
-            this.btnReplaceProcName.UseVisualStyleBackColor = true;
-            this.btnReplaceProcName.Click += new System.EventHandler(this.btnReplaceProcName_Click);
+            btnReplaceProcName.Location = new System.Drawing.Point(13, 78);
+            btnReplaceProcName.Name = "btnReplaceProcName";
+            btnReplaceProcName.Size = new System.Drawing.Size(300, 26);
+            btnReplaceProcName.TabIndex = 2;
+            btnReplaceProcName.Text = "Replace AOB .exe to $process";
+            btnReplaceProcName.UseVisualStyleBackColor = true;
+            btnReplaceProcName.Click += btnReplaceProcName_Click;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(456, 296);
-            this.Controls.Add(this.btnReplaceProcName);
-            this.Controls.Add(this.btnIDRenumber);
-            this.Controls.Add(this.btnAAToggleGen);
-            this.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.MaximizeBox = false;
-            this.Name = "MainForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "CE AA Script helper";
-            this.ResumeLayout(false);
+            AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            ClientSize = new System.Drawing.Size(322, 157);
+            Controls.Add(btnReplaceProcName);
+            Controls.Add(btnIDRenumber);
+            Controls.Add(btnAAToggleGen);
+            Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 136);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Margin = new System.Windows.Forms.Padding(4);
+            MaximizeBox = false;
+            Name = "MainForm";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "CE AA Script helper";
+            ResumeLayout(false);
 
         }
 
